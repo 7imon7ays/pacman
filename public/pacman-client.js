928c33e16a5903c326ff17ca78b0f99bb28b7bb3
Simon Chaffetz <simis1000@gmail.com>
2013-12-25 10:23:17 +0800

    Draw statis left/right mouth

--- public/pacman-client.js
+++ public/pacman-client.js
@@ -3,12 +3,23 @@ function Pacman (id) {
 }
 
 Pacman.prototype.render = function (canvas) {
-  canvas.fillStyle   = "FF0000";
+  canvas.fillStyle = "FF0000";
   canvas.strokeStyle = "FF0000";
-
   canvas.beginPath();
   canvas.arc(this.x, this.y, this.size, 0, Math.PI*2, true);
   canvas.closePath();
   canvas.fill();
   canvas.stroke();
+
+  canvas.fillStyle = "fff";
+  canvas.strokeStyle = "fff";
+  canvas.beginPath();
+  canvas.moveTo(this.x, this.y);
+
+  canvas.lineTo(this.x + 6 * this.xDelta, this.y - this.size - 2);
+  canvas.lineTo(this.x + 6 * this.xDelta, this.y + this.size - 2);
+
+  canvas.lineTo(this.x, this.y);
+  canvas.closePath();
+  canvas.fill();
 }
