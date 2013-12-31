18c4a8c55f2ffa798ef2cadce95568d007598c69
Simon Chaffetz <simis1000@gmail.com>
2013-12-31 10:02:57 +0800

    Animate mouth

--- public/pacman-client.js
+++ public/pacman-client.js
@@ -2,24 +2,29 @@ function Pacman (id) {
   this.id = id;
 }
 
-Pacman.prototype.render = function (canvas) {
-  canvas.fillStyle = "FF0000";
-  canvas.strokeStyle = "FF0000";
-  canvas.beginPath();
-  canvas.arc(this.x, this.y, this.size, 0, Math.PI*2, true);
-  canvas.closePath();
-  canvas.fill();
-  canvas.stroke();
+Pacman.prototype.render = function (context) {
+  context.fillStyle = "FF0000";
+  context.strokeStyle = "FF0000";
+  context.beginPath();
+  context.arc(this.x, this.y, this.size, 0, Math.PI*2, true);
+  context.closePath();
+  context.fill();
+  context.stroke();
 
-  canvas.fillStyle = "fff";
-  canvas.strokeStyle = "fff";
-  canvas.beginPath();
-  canvas.moveTo(this.x, this.y);
+  context.moveTo(this.x, this.y);
+  context.fillStyle = "fff";
+  context.strokeStyle = "fff";
+  context.beginPath();
 
-  canvas.lineTo(this.x + 6 * this.xDelta, this.y - this.size - 2);
-  canvas.lineTo(this.x + 6 * this.xDelta, this.y + this.size - 2);
+  var upperLipX = this.x + 6 * this.xDelta;
+  var upperLipY = this.y - this.x % 8;
+  var lowerLipX = this.x + 6 * this.xDelta;
+  var lowerLipY = this.y + this.x % 8;
 
-  canvas.lineTo(this.x, this.y);
-  canvas.closePath();
-  canvas.fill();
+  context.lineTo(upperLipX, upperLipY);
+  context.lineTo(lowerLipX, lowerLipY);
+
+  context.lineTo(this.x, this.y);
+  context.closePath();
+  context.fill();
 }
