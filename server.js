9deffb483d0dcf35fc3bcbf29d89c63967ab3393
Simon Chaffetz <simis1000@gmail.com>
2014-01-01 22:57:09 +0800

    Make server object-oriented

--- server.js
+++ server.js
@@ -1,65 +1,38 @@
-var _ = require("underscore");
-var app = require("http").createServer(handler);
-var io = require("socket.io").listen(app);
-var fs = require("fs");
-var Game = require("./game_logic/game-server");
-var game;
-var playerCount = 0;
-var port = process.env.PORT || 3000;
+var _ = require("underscore")
+  , http = require("http")
+  , io = require("socket.io")
+  , fs = require("fs")
+  , Game = require("./game_logic/game-server");
 
-
-app.listen(port);
-io.sockets.on("connection", function(socket) {
-  listenForGameLoad(socket);
-});
-
-function handler (req, res) {
-  switch (req.url) {
-    case "/":
-      render("public/chat-room.html", res);
-      break;
-    case "/game-client.js":
-      render("public/game-client.js", res);
-      break;
-    case "/game-port":
-      setPort(res);
-      break;
-    case "/game-room.html":
-      console.log("\n\n\n\n\n\n")
-      console.log(req)
-      console.log("\n\n\n\n\n\n")
-      render("public/game-room.html", res);
-      break;
-    case "/grid-client.js":
-      render("public/grid-client.js", res);
-      break;
-    case "/pacman-client.js":
-      render("public/pacman-client.js", res);
-      break;
-      case "/pacman-style.css":
-        render("public/pacman-style.css", res);
-      break;
-    default:
-      res.writeHead(404);
-      res.end("Not found");
-      break;
-  }
+function Server () {
+  this.port = process.env.PORT || 3000;
 }
 
-function listenForGameLoad(socket) {
+Server.prototype.run = function () {
+  var self = this;
+  this.app = http.createServer(this.handler.bind(this));
+  this.io = io.listen(this.app);
+  this.app.listen(this.port);
+  this.io.on("connection", function(socket) {
+    self.listenForGameLoad(socket);
+  });
+}
+
+Server.prototype.listenForGameLoad = function (socket) {
+  var self = this;
   socket.on("gameLoaded", function () {
-    if (game) {
-      game.addPlayer(socket);
+    if (self.game) {
+      self.game.addPlayer(socket);
     } else {
       var canvasDimensions = { height: 300, width: 500 };
       var pacmanSpeed = 2;
-      game = new Game(pacmanSpeed, canvasDimensions);
-      game.start(socket);
+      self.game = new Game(pacmanSpeed, canvasDimensions);
+      self.game.start(socket);
     }
   });
 }
 
-function render(filename, res) {
+Server.prototype.render = function (filename, res) {
   fs.readFile(filename, function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -71,7 +44,42 @@ function render(filename, res) {
   });
 }
 
-function setPort(res) {
+Server.prototype.declarePort = function (res) {
   res.writeHead(200);
-  res.end(port.toString());
+  res.end(this.port.toString());
 }
+
+Server.prototype.handler = function (req, res) {
+  switch (req.url) {
+    case "/":
+      this.render("public/chat-room.html", res);
+      break;
+    case "/game-client.js":
+      this.render("public/game-client.js", res);
+      break;
+    case "/game-port":
+      this.declarePort(res);
+      break;
+    case "/game-room.html":
+      console.log("\n\n\n\n\n\n")
+      console.log(req)
+      console.log("\n\n\n\n\n\n")
+      this.render("public/game-room.html", res);
+      break;
+    case "/grid-client.js":
+      this.render("public/grid-client.js", res);
+      break;
+    case "/pacman-client.js":
+      this.render("public/pacman-client.js", res);
+      break;
+    case "/pacman-style.css":
+        this.render("public/pacman-style.css", res);
+      break;
+    default:
+      res.writeHead(404);
+      res.end("Not found");
+      break;
+  }
+}
+
+new Server().run();
