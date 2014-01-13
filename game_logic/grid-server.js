6ab4441459d325f9ec0b4ee32c7b78f290fbf84f
Simon Chaffetz <simis1000@gmail.com>
2014-01-12 20:55:00 -0800

    Build out grid

--- game_logic/grid-server.js
+++ game_logic/grid-server.js
@@ -1,66 +1,266 @@
 var grid = {
   
-  // top t
+  // left wall
   
+  leftTopWall: {
+    x: 0,
+    y: 0,
+    width: 4,
+    height: 130
+  },
+  
+  leftBottomWall: {
+    x: 0,
+    y: 170,
+    width: 4,
+    height: 130
+  },
+
+
+
+
+  // right wall
+  
+  rightTopWall: {
+    x: 496,
+    y: 0,
+    width: 4,
+    height: 130
+  },
+  
+  rightBottomWall: {
+    x: 496,
+    y: 170,
+    width: 4,
+    height: 130
+  },
+
+
+
+
+  // side tubes
+  
+  leftTubeTop: {
+    x: 0,
+    y: 130,
+    width: 40,
+    height: 4
+  },
+  
+  leftTubeBottom: {
+    x: 0,
+    y: 170,
+    width: 40,
+    height: 4
+  },
+  
+  rightTubeTop: {
+    x: 460,
+    y: 130,
+    width: 40,
+    height: 4
+  },
+  
+  rightTubeBottom: {
+    x: 460,
+    y: 170,
+    width: 40,
+    height: 4
+  },
+
+
+
+
+  // top and bottom wall
+  
+  topWall: {
+    x: 4,
+    y: 0,
+    width: 492,
+    height: 4
+  },
+
+  topWallStem : {
+    x: 246,
+    y: 0,
+    width: 4,
+    height: 30
+  },
+  
+  bottomWall: {
+    x: 4,
+    y: 296  ,
+    width: 492,
+    height: 4
+  },
+
+
+  
+  // Vertical Ts
+    
   topTBase: {
     x: 200,
-    y: 0,
+    y: 60,
     width: 100,
     height: 4
   },
   
   topTStem: {
-    x: 250,
-    y: 0,
+    x: 246,
+    y: 60,
     width: 4,
     height: 30
   },
   
-  // bottom t
-  
-  bottomTBase: {
+  midTBase: {
     x: 200,
-    y: 296  ,
+    y: 200,
     width: 100,
     height: 4
   },
   
-  bottomTStem: {
-    x: 250,
-    y: 266,
+  midTStem: {
+    x: 246,
+    y: 200,
     width: 4,
     height: 30
   },
   
+  bottomLineBase: {
+    x: 200,
+    y: 260,
+    width: 100,
+    height: 4
+  },
+
+
+
+
+  // left Ls
+  
+  topLeftLStick: {
+    x: 40,
+    y: 60,
+    width: 100,
+    height: 4
+  },
+  
+  topLeftTSide: {
+    x: 40,
+    y: 30,
+    width: 4,
+    height: 60
+  },
+  
+  bottomLeftLStick: {
+    x: 40,
+    y: 240,
+    width: 100,
+    height: 4
+  },
+  
+  bottomLeftTSide: {
+    x: 40,
+    y: 210,
+    width: 4,
+    height: 60
+  },
+
+
+
+  // right Ls
+  
+  topRightTSide: {
+    x: 452,
+    y: 30,
+    width: 4,
+    height: 60
+  },
+  
+  topRightLStick: {
+    x: 352,
+    y: 60,
+    width: 100,
+    height: 4
+  },
+  
+  bottomRightTSide: {
+    x: 452,
+    y: 210,
+    width: 4,
+    height: 60
+  },
+  
+  bottomRightLStick: {
+    x: 352,
+    y: 240,
+    width: 100,
+    height: 4
+  },  
+  
+  
+  
+  // middle Ls
+  
+  middleLeftLSide: {
+    x: 80,
+    y: 130,
+    width: 4,
+    height: 40
+  },
+  
+  middleLeftLStick: {
+    x: 80,
+    y: 148,
+    width: 60,
+    height: 4
+  },
+  
+  middleRightLSide: {
+    x: 416,
+    y: 130,
+    width: 4,
+    height: 40
+  },
+  
+  middleRightLStick: {
+    x: 356,
+    y: 148,
+    width: 60,
+    height: 4
+  },
+
+
   // middle rectangle
   
   midRight: {
     x: 300,
-    y: 100,
+    y: 120,
     width: 4,
     height: 54
   },
   
   midTop: {
     x: 200,
-    y: 100,
+    y: 120,
     width: 100,
     height: 4
   },
   
   midLeft: {
     x: 200,
-    y: 100,
+    y: 120,
     width: 4,
     height: 50
   },
   
   midBottom: {
     x: 200,
-    y: 150,
+    y: 170,
     width: 100,
     height: 4
   }
+  
 };
 
 module.exports = grid;
